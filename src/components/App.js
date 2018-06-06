1bfaa4f5a2f2d98d7141c00ea0198191799efda1
Sean Luckett <whitecrane007@gmail.com>
2018-06-06 17:17:10 -0600

    WIP: Content (some styles) for recipe list page

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,28 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Recipes</h1>
+      <div>
+        <section className='titlebar'>
+          <h1 className='header'>Cookable</h1>
+        </section>
+        <section className="main-content">
+          <h1 className="section-title">Recipes</h1>
+          <div className="recipe-list">
+            <div className="column-headers">
+              <h2>Title</h2>
+              <h2>Type</h2>
+              <h2>Prep time</h2>
+              <h2>Source</h2>
+            </div>
+            <div className="recipe-listings">
+              <div>Some Title</div>
+              <div>Entree</div>
+              <div>Under 30 min</div>
+              <div>Epicurious</div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/main.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
